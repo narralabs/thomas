80fffe3b2df9c910508c2853092fbcf060debeb9
William Estoque <william.estoque@gmail.com>
2022-03-17 14:30:50 -0400

    Add devise User. Add git initial commit.

--- template.rb
+++ template.rb
@@ -2,4 +2,9 @@ gem "devise"
 
 after_bundle do
   rails_command("generate devise:install")
+  rails_command("generate devise User")
+
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
 end
